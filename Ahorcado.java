3d3665052988afc8edc333c18fd5a00b9cacadcc
elias <eruedaarevalo@alumno.unlam.edu.ar>
2021-04-09 19:24:26 -0300

    Cambie el if por un switch

--- Ahorcado.java
+++ Ahorcado.java
@@ -20,16 +20,20 @@ public class Ahorcado {
         a = teclado.nextInt();
         System.out.println("Ingrese el segundo numero");
         b = teclado.nextInt();
-        if(opcion.equals(1)){
+        switch(opcion) {
+        case 1:
             resultado = a + b;
-        }else if(opcion.equals(2)){
+            break;
+        case 2:
             resultado = a - b;
-        }else if(opcion.equals(3)){
+        break;
+        case 3:
             resultado = a * b;
-        }else{
+            break;
+        case 4:
             resultado = a / b;
         }
-        System.out.println("El resultado del ahorcado es " + resultado.toString());
+        System.out.println("El resultado es " + resultado.toString());
     }
 
 }
